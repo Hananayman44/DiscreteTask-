0c4e8871ebc0b7f84bed1f16b7e166f9f5a372da
Hananayman44 <121646501+Hananayman44@users.noreply.github.com>
2022-12-30 16:41:51 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROJECT_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, sum, i;
+            for (number = 29; number <= 9000; number++)
+            {
+                i = 1;
+                sum = 0;
+                while (i < number)
+                {
+                    if (number % i == 0)
+                        sum = sum + i;
+                    i++;
+                }
+                if (sum == number)
+                {
+                    Console.WriteLine("perfect number is " + number);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
+     
+
+
+
+
+                   
+                        
+                       
+              
+   
+               
+               
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
